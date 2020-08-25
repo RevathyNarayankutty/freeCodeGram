530752aa571f739960cb24a1caa482dcb18ccb42
RevathyNarayankutty <revathynarayankutty@gmail.com>
2020-08-25 14:35:28 +0530

    added few new functionalities

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,44 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="row">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <div class="col-3 p-5">
+            <img src="https://instagram.fcok6-1.fna.fbcdn.net/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=instagram.fcok6-1.fna.fbcdn.net&_nc_ohc=EEQDWsUFAwIAX_89Djt&oh=21fae74b688f79edf8923dac38a83207&oe=5F6E5B67" class="rounded-circle">
+        </div>
 
-                    {{ __('You are logged in!') }}
-                </div>
+        <div class="col-9 pt-5">
+
+            <div><h1>freeCodeGram</h1></div>
+
+            <div class= "d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
             </div>
+
+            <div class= "pt-4 font-weight-bold">freecodegram.org<div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.<div>
+            <div><a href="#">www.freecodecamp.org</a><div>
+            
+    
         </div>
     </div>
+
+    <div class="row pt-5">
+
+        <div class="col-4">
+            <img src="https://marketingtechnews.net/wp-content/uploads/sites/6/2020/03/3d-emojis-icons-with-facial-expressions-picture-id1049005764.jpg" class="w-100">
+        </div>
+
+        <div class="col-4">
+            <img src="https://i.pinimg.com/600x315/f2/aa/2f/f2aa2f9be5197d8a197c22790079f5e8.jpg" class="w-100">
+        </div>
+
+        <div class="col-4">
+            <img src="https://icdn4.digitaltrends.com/image/digitaltrends/apple-new-emojis-yawning-2-500x300-c.jpg" class="w-100">
+        </div>
+    </div>
+
 </div>
 @endsection
