60f63652bc1e403479b51c8b12de44ef5ffa6b31
Revathi Naraynkutty <revathi@mosaique.link>
2021-12-16 11:56:34 +0530

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO-hjco1YzPVQPhzVqep-Dt7akp1djlm0Zg1sQPza3dMkrfeqNIZJXiOMjJGpCm06zBn8&usqp=CAU" class="rounded-circle">
+        </div>
+
+        <div class="col-9 pt-5">
+            <div><h1>freeCodeCamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>123</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
